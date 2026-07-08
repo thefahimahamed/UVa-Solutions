c211e707c83c7d00bb09c39d8e0dd9e6d9074115
Fahim Ahamed <fahimahamed1822@email.com>
2026-07-08 16:14:52 +0600

    Add UVa 10970 - Big Chocolate

---
+++ 10970-Big-Chocolate/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+  
+int main()
+{
+  pair < long long int , long long int > p;
+  while(cin >> p.first >> p.second)
+  {
+    
+    long long int ans = (p.first-1)+(p.first*(p.second-1));
+    cout << ans << "\n";
+  }
+    
+    
+    
+  
+  
+}
